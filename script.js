aaed56bc29fc82fbd30eae07a1f0b0460a6848ca
Spencer <spencer676@hotmail.com>
2020-12-07 20:12:59 +0100

    js

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const container = document.querySelector(".container");
-const submitButton = document.getElementById("submit-button");
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-const resultlist = document.getElementById("results");
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
 
-submitButton.addEventListener("click", (e) => {
-  if (input.value === "") {
-    output.value = "Please enter a value";
-  } else {
-    output.value = input.innerText;
-    resultlist.value = input.innerText;
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+console.log(selectedWord);
+
+const correctLetter = [];
+const wrongLetters = [];
+
+//Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+  ${selectedWord
+    .split("")
+    .map(
+      (letter) => `
+  <span class="letter">
+  ${correctLetter.includes(letter) ? letter : ""}
+  </span>
+  `
+    )
+    .join("")}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won!";
+    popup.style.display = "flex";
   }
-});
+}
 
-resultlist.addEventListener("change", (e) => {});
+displayWord();
